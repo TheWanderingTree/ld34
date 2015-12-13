a01057c81bca287e4f8f40ef7fd29f7986ad64ff
TheWanderingTree <soren.laulainen@gmail.com>
2015-12-12 19:29:22 -0800

    Added some audio events to Main scene
    - Generated MAIN soundbank, loaded by Wwise Global object
    - bigDog prefab has audio listener, and startBigDog event
    - Environment Camera prefab has playDesertAmb event
    - drone prefab has droneSpawn event

---
+++ New Unity Project/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BAGJOSTLE = 2229533828U;
+        static const AkUniqueID BUFFKICKERATTACK = 614269825U;
+        static const AkUniqueID DESTROYDRONE_EXPLODE = 823393987U;
+        static const AkUniqueID DESTROYDRONE_SPINOUT = 1088977592U;
+        static const AkUniqueID DRONESPAWN = 690362850U;
+        static const AkUniqueID PLAYDESERTAMB = 3938763508U;
+        static const AkUniqueID STARTBIGDOG = 3499504239U;
+        static const AkUniqueID STOPBIGDOG = 2195046349U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace ENGINE
+        {
+            static const AkUniqueID GROUP = 268529915U;
+
+            namespace STATE
+            {
+                static const AkUniqueID RUNNING = 3863236874U;
+                static const AkUniqueID STOPPED = 2904797076U;
+            } // namespace STATE
+        } // namespace ENGINE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace DRONESTATE
+        {
+            static const AkUniqueID GROUP = 3733145084U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+            } // namespace SWITCH
+        } // namespace DRONESTATE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BIGDOG_SPEED = 3395678809U;
+        static const AkUniqueID DISTANCE = 1240670792U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ENEMIES = 2242381963U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
